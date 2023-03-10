ca12fd68a18fd3add3fb9e9d8d04e3b33c7ac6a2
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-10 01:12:54 +0100

    wip

--- src/Models/Question.php
+++ src/Models/Question.php
@@ -5,7 +5,6 @@ namespace QRFeedz\Cube\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use QRFeedz\Cube\Relations\LastVersionWidgets;
 
 class Question extends Model
 {
