eb86f028774a7b38dc9387fc8c1db1c74694842b
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-16 22:33:47 +0100

    Update Locale.php

--- src/Models/Locale.php
+++ src/Models/Locale.php
@@ -4,7 +4,6 @@ namespace QRFeedz\Cube\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use QRFeedz\Cube\Models\QuestionWidget;
 
 class Locale extends Model
 {
