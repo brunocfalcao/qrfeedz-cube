c18124ca433d0e1e7cee4aea97050c279e264da7
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-01 18:00:35 +0200

    wip

--- src/Observers/GroupObserver.php
+++ src/Observers/GroupObserver.php
@@ -6,50 +6,7 @@ use QRFeedz\Cube\Models\Group;
 
 class GroupObserver
 {
-    /**
-     * Handle the Group "saving" event.
-     */
     public function saving(Group $model)
-    {
-        //
-    }
-
-    /**
-     * Handle the Group "created" event.
-     */
-    public function created(Group $model)
-    {
-        //
-    }
-
-    /**
-     * Handle the Group "updated" event.
-     */
-    public function updated(Group $model)
-    {
-        //
-    }
-
-    /**
-     * Handle the Group "deleted" event.
-     */
-    public function deleted(Group $model)
-    {
-        //
-    }
-
-    /**
-     * Handle the Group "restored" event.
-     */
-    public function restored(Group $model)
-    {
-        //
-    }
-
-    /**
-     * Handle the Group "force deleted" event.
-     */
-    public function forceDeleted(Group $model)
     {
         //
     }
